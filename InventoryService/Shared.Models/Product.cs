eacb7a064b272b26ee43e73c7e06842db0774c13
Tavilo Breno <breno_wk2@hotmail.com>
2026-04-09 16:14:14 -0300

    feat: add InventoryService project with Application, Domain, and Infrastructure layers; implement Product and Invoice models

---
+++ InventoryService/Shared.Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Shared.Models
+{
+    public class Product
+    {
+       public int Id { get; set; }
+       public string? Code { get; set; }
+       public string? Description { get; set; }
+       public int StockBalance { get; set; }
+
+        [Timestamp]
+        public byte[]? RowVersion { get; set; }
+
+        public ICollection<InvoiceItem> InvoiceItems { get; set; } = new List<InvoiceItem>();
+    }
+}
