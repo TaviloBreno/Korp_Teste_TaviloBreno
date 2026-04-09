956fa4c179d3d53481c297580b3c98b67bae30db
Tavilo Breno <breno_wk2@hotmail.com>
2026-04-09 16:25:38 -0300

    feat: add IProductRepository interface for product data access operations

---
+++ InventoryService/InventoryService.Domain/Repositories/IProductRepository.cs
@@ -0,0 +1,19 @@
+﻿using InventoryService.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryService.Domain.Repositories
+{
+    public interface IProductRepository
+    {
+        Task<Product?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+        Task<Product?> GetByCodeAsync(string code, CancellationToken cancellationToken = default);
+        Task<IEnumerable<Product>> GetAllAsync(CancellationToken cancellationToken = default);
+        Task AddAsync(Product product, CancellationToken cancellationToken = default);
+        void Update(Product product);
+        Task<bool> ExistsByCodeAsync(string code, CancellationToken cancellationToken = default);
+    }
+}
