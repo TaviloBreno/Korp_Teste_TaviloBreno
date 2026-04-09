6634f366b64011582ba041082efcdcc834e4ce12
Tavilo Breno <breno_wk2@hotmail.com>
2026-04-09 18:41:50 -0300

    feat: add BillingDbContext for managing Invoice and InvoiceItem entities

---
+++ InventoryService/BillingService.Infrastructure/Data/BillingDbContext.cs
@@ -0,0 +1,35 @@
+﻿using BillingService.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BillingService.Infrastructure.Data
+{
+    public class BillingDbContext : DbContext
+    {
+        public BillingDbContext(DbContextOptions<BillingDbContext> options) : base(options) { }
+
+        public DbSet<Invoice> Invoices => Set<Invoice>();
+        public DbSet<InvoiceItem> InvoiceItems => Set<InvoiceItem>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Invoice>(entity =>
+            {
+                entity.HasKey(i => i.Id);
+                entity.Property(i => i.SequentialNumber).IsRequired();
+                entity.Property(i => i.Status).IsRequired();
+                entity.Property(i => i.TotalAmount).HasColumnType("decimal(18,2)");
+                entity.HasIndex(i => i.SequentialNumber).IsUnique();
+                entity.HasMany(i => i.Items).WithOne().HasForeignKey(i => i.InvoiceId);
+            });
+
+            modelBuilder.Entity<InvoiceItem>(entity =>
+            {
+                entity.HasKey(i => i.Id);
+                entity.Property(i => i.Quantity).IsRequired();
+                entity.Property(i => i.UnitPrice).HasColumnType("decimal(18,2)").IsRequired();
+            });
+        }
+    }
+}
