4eab81f086994390c84bfff508f3707a02bf5c18
Tavilo Breno <breno_wk2@hotmail.com>
2026-04-09 18:41:15 -0300

    feat: add InvoiceDto and related records for invoice management

---
+++ InventoryService/BillingService.Application/DTOs/InvoiceDto.cs
@@ -0,0 +1,36 @@
+﻿using BillingService.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BillingService.Application.DTOs
+{
+    public record InvoiceDto(
+    Guid Id,
+    int SequentialNumber,
+    InvoiceStatus Status,
+    DateTime CreatedAt,
+    DateTime? ClosedAt,
+    decimal TotalAmount,
+    IEnumerable<InvoiceItemDto> Items
+);
+
+    public record CreateInvoiceDto(
+        IEnumerable<CreateInvoiceItemDto> Items
+    );
+
+    public record CreateInvoiceItemDto(
+        Guid ProductId,
+        int Quantity,
+        decimal UnitPrice
+    );
+
+    public record InvoiceItemDto(
+        Guid Id,
+        Guid ProductId,
+        int Quantity,
+        decimal UnitPrice
+    );
+}
