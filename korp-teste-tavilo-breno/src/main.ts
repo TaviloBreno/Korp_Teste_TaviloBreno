62cccc7797b04a97dcb37c322b859ae1129f6ecf
Tavilo Breno <breno_wk2@hotmail.com>
2026-04-10 14:16:54 -0300

    feat: add MSW for API mocking and refactor main application bootstrap logic

--- korp-teste-tavilo-breno/src/main.ts
+++ korp-teste-tavilo-breno/src/main.ts
@@ -1,6 +1,18 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
-import { App } from './app/app';
+import { AppComponent } from './app/app';
+import { environment } from './environments/environment';
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+async function setupApp() {
+  // Ativa mock apenas em desenvolvimento
+  if (!environment.production) {
+    const { worker } = await import('./mocks/browser');
+    await worker.start({ onUnhandledRequest: 'bypass' });
+  }
+
+  bootstrapApplication(AppComponent, appConfig).catch((err) =>
+    console.error('Erro ao inicializar aplicação:', err),
+  );
+}
+
+setupApp();
