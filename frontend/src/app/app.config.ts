27975fd3b008caa51c4576333af5ba4961273d50
Tavilo Breno <breno_wk2@hotmail.com>
2026-04-10 17:02:36 -0300

    feat: add initial application configuration, main layout, and global styles

---
+++ frontend/src/app/app.config.ts
@@ -0,0 +1,32 @@
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import { MessageService } from 'primeng/api';
+import Aura from '@primeng/themes/aura';
+import { routes } from './app.routes';
+import { loadingInterceptor } from './core/interceptors/loading.interceptor';
+import { errorInterceptor } from './core/interceptors/error.interceptor';
+import { idempotencyInterceptor } from './core/interceptors/idempotency.interceptor';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    MessageService,
+    provideHttpClient(
+      withInterceptors([loadingInterceptor, errorInterceptor, idempotencyInterceptor]),
+    ),
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+        options: {
+          darkModeSelector: false,
+          cssLayer: false,
+        },
+      },
+    }),
+  ],
+};
