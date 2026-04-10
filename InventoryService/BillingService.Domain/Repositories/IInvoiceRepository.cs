703cfff9c092e1c3a6af6108d90e3e7b235823b6
Tavilo Breno <breno_wk2@hotmail.com>
2026-04-09 21:12:14 -0300

    feat: remove unused usings from IInvoiceRepository and ensure proper package reference in BillingService.Domain.csproj

--- InventoryService/BillingService.Domain/Repositories/IInvoiceRepository.cs
+++ InventoryService/BillingService.Domain/Repositories/IInvoiceRepository.cs
@@ -1,9 +1,4 @@
 ﻿using BillingService.Domain.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BillingService.Domain.Repositories
 {
